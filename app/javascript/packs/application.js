41bcc784fc1199cfdce33e6e2e2e29489e61d2f7
Simon George <simon@sfcgeorge.co.uk>
2019-10-28 12:00:10 +0000

    Import core-js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,6 +2,8 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import "core-js/stable";
+import "regenerator-runtime/runtime";
 
 require("@rails/ujs").start();
 require("turbolinks").start();
@@ -11,6 +13,8 @@ require("channels");
 document.querySelectorAll("*").forEach(function() {
   console.log(e.innerHTML);
 });
+const set = new Set([1, 2, 3]);
+[1, 2, 3].includes(2);
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
